2ab8c729c2adc7a7316f52f57556c4b1e04f7ba3
jangsen <dh7895210@naver.com>
2025-02-19 18:59:05 +0900

    Add rabbitmq

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService: ConfigService = app.get(ConfigService);
 
   // cors
   app.enableCors();
@@ -28,6 +31,20 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, documentFactory);
+
+  // RabbitMQ
+  const RABBITMQ_URL = configService.get('RABBITMQ_URL');
+  const microservice = app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [RABBITMQ_URL],
+      queue: 'order_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+  await microservice.listen();
 
   await app.listen(process.env.PORT ?? 8000);
 }
--- src/rabbitmq/rabbitmq.module.ts
+++ src/rabbitmq/rabbitmq.module.ts
@@ -1,4 +1,57 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        name: 'PRODUCT_SERVICE',
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            url: [configService.get<string>('RABBITMQ_URL')],
+            queue: 'product_queue',
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+      },
+      {
+        name: 'CUSTOMER_SERVICE',
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            url: [configService.get<string>('RABBITMQ_URL')],
+            queue: 'customer_queue',
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+      },
+      {
+        name: 'ORDER_SERVICE',
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            url: [configService.get<string>('RABBITMQ_URL')],
+            queue: 'order_queue',
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+      },
+    ]),
+  ],
+  exports: [ClientsModule],
+})
 export class RabbitmqModule {}
